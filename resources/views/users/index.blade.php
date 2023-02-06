2a8a6aef24f1c94d6c99e0b48e5c2b85e2b61e6c
Calof <calofgabriel@gmail.com>
2023-02-06 18:37:15 -0300

    Formatando a pagina

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -6,8 +6,8 @@
     <a href="{{route('users.create')}}" class="bg-blue-900 rounded-full text-white px-4 text-sm">+</a>
 </h1>
 <form action="{{route('users.index')}}" method="get" class="py-5">
-<input type="text" name="search" placeholder="Pesquisar" class="md:w-1/6 bg-gray-200 appearance-none">
-    <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline rounded-full text-white box-content h-2 w-17 p-5  " >Pesquisar</button>
+<input type="text" name="search" placeholder="Pesquisar" class="md:w-1/6 bg-gray-200 appearance-none box-content h-100 w-100 p-2 rounded-lg  ">
+    <button class="shadow bg-purple-300 hover:bg-purple-200 focus:shadow-outline rounded-lg box-content h-100 w-100 p-2" >Pesquisar</button>
 <ul>
     
     
@@ -42,10 +42,10 @@
             <td class="px-5 py-5 border-b border-gray-100">{{ $user->name }}</td>
             <td class="px-5 py-5 border-b border-gray-100">{{ $user->email }}</td>
             <td class="px-5 py-5 border-b border-gray-100">
-                <a href="{{ route('users.show', $user->id) }}" class="shadow bg-blue-300 hover:bg-blue-200 focus:shadow-outline rounded-full box-content h-32 w-32 p-4 ">Detalhes</a>
+                <a href="{{ route('users.show', $user->id) }}" class="shadow bg-blue-300 hover:bg-blue-200 focus:shadow-outline rounded-lg box-content h-100 w-100 p-2">Detalhes</a>
             </td>
             <td class="px-5 py-5 border-b border-gray-100">
-                <a href="{{ route('user.edit', $user->id) }}" class="shadow bg-green-300 hover:bg-green-200 focus:shadow-outline rounded-full box-content h-32 w-32 p-4   " >Editar</a>
+                <a href="{{ route('user.edit', $user->id) }}" class="shadow bg-green-300 hover:bg-green-200 focus:shadow-outline rounded-lg box-content h-100 w-100 p-2" >Editar</a>
             </td>
         </tr>
         @endforeach
--- resources/views/users/partials/form.blade.php
+++ resources/views/users/partials/form.blade.php
@@ -1,8 +1,8 @@
 @csrf
 
-<input type="text" name="name" placeholder="Nome:" value="{{$user->name ?? old('name')}}">
-<input type="email" name="email" placeholder="E-mail:" value="{{$user->email ?? old('email')}}">
-<input type="password" name="password" placeholder="Senha:">
+<input type="text" name="name" placeholder="Nome:" value="{{$user->name ?? old('name')}}" class="md:w-1/6 bg-gray-200 appearance-none box-content h-100 w-100 p-2 rounded-lg  ">
+<input type="email" name="email" placeholder="E-mail:" value="{{$user->email ?? old('email')}}" class="md:w-1/6 bg-gray-200 appearance-none box-content h-100 w-100 p-2 rounded-lg  " >
+<input type="password" name="password" placeholder="Senha:" class="md:w-1/6 bg-gray-200 appearance-none box-content h-100 w-100 p-2 rounded-lg  " >
 <button type="submit" class="shadow bg-green-300 hover:bg-green-200 focus:shadow-outline rounded-lg box-content h-100 w-100 p-2">
     Enviar
 </button>
